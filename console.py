6036bfca766c16b22bc0db993127b6dcdbe84d32
kema-ray <rachelkemuma99@gmail.com>
2023-03-12 21:09:29 +0300

    make changes to the console file

--- console.py
+++ console.py
@@ -197,26 +197,10 @@ class HBNBCommand(cmd.Cmd):
         setattr(store[key], attr_k, attr_j)
         storage.save()
 
-    def parse(arg):
-        curly_braces = re.search(r"\{(.*?)\}", arg)
-        brackets = re.search(r"\[(.*?)\]", arg)
-        if curly_braces is None:
-            if brackets is None:
-                return [i.strip(",") for i in split(arg)]
-            else:
-                lexer = split(arg[:brackets.span()[0]])
-                retl = [i.strip(",") for i in lexer]
-                retl.append(brackets.group())
-                return retl
-        else:
-            lexer = split(arg[:curly_braces.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(curly_braces.group())
-            return retl
-
     def do_count(self, args):
-        """"""
-        # arg1 = parse(args)
+        """
+        retrieves the number of instances of a class
+        """
         count = 0
         for obj in storage.all().values():
             if obj.__class__.__name__ == args:
